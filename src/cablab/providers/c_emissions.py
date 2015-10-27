3585031e660c383a94cb78f8e9be992069b560de
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2015-10-27 13:48:58 +0100

    refactoring

--- src/cablab/providers/c_emissions.py
+++ src/cablab/providers/c_emissions.py
@@ -19,49 +19,49 @@ class CEmissionsProvider(BaseImageProvider):
         self.temp_path = os.path.join(cache_dir, os.path.basename(self.dir_path))
         self.old_indices = None
 
-    def compute_images_from_sources(self, source_indices_to_time_overlap):
+    def compute_images_from_sources(self, index_to_weight):
 
         # close all datasets that wont be used anymore
-        new_indices = source_indices_to_time_overlap.keys()
+        new_indices = set(index_to_weight.keys())
         if self.old_indices:
             unused_indices = self.old_indices - new_indices
             for i in unused_indices:
                 file, _ = self.time_range_to_file[i]
-                if file in self.open_datasets:
-                    self.open_datasets[file].close()
-                    del self.open_datasets[file]
+                self._close_dataset(file)
 
         self.old_indices = new_indices
 
-        emissions_sum = numpy.zeros((self.cube_config.grid_height, self.cube_config.grid_width), dtype=numpy.float32)
-        weight_sum = 0.0
-        for i in new_indices:
-            weight = source_indices_to_time_overlap[i]
+        if len(new_indices) == 1:
+            i = next(iter(new_indices))
             file, time_index = self.time_range_to_file[i]
+            dataset = self._get_dataset(file)
+            emissions = dataset.variables['C_Emissions'][time_index, :, :]
+        else:
+            emissions_sum = numpy.zeros((self.cube_config.grid_height, self.cube_config.grid_width), dtype=numpy.float32)
+            weight_sum = 0.0
+            for i in new_indices:
+                weight = index_to_weight[i]
+                file, time_index = self.time_range_to_file[i]
+                dataset = self._get_dataset(file)
+                emissions = dataset.variables['C_Emissions']
+                emissions_sum += weight * emissions[time_index, :, :]
+                weight_sum += weight
+            emissions = emissions_sum / weight_sum
 
-            if file in self.open_datasets:
-                dataset = self.open_datasets[file]
+        return {'C_Emissions': emissions}
+
+    def _get_dataset(self, file):
+        if file in self.open_datasets:
+            dataset = self.open_datasets[file]
+        else:
+            root, ext = os.path.splitext(file)
+            if ext == '.gz':
+                real_file = self._get_unpacked_file(file)
             else:
-                root, ext = os.path.splitext(file)
-                if ext == '.gz':
-                    # Unpack to temp_path, if it is gzipped
-                    real_file = os.path.join(self.temp_path, os.path.basename(root))
-                    if not os.path.exists(real_file):
-                        if not os.path.exists(self.temp_path):
-                            os.makedirs(self.temp_path, exist_ok=True)
-                        with gzip.open(file, 'rb') as istream:
-                            with open(real_file, 'wb') as ostream:
-                                ostream.write(istream.read())
-                else:
-                    real_file = file
-                dataset = netCDF4.Dataset(real_file)
-                self.open_datasets[file] = dataset
-
-            emissions = dataset.variables['C_Emissions']
-            emissions_sum += weight * emissions[time_index, :, :]
-            weight_sum += weight
-
-        return {'C_Emissions': emissions_sum / weight_sum}
+                real_file = file
+            dataset = netCDF4.Dataset(real_file)
+            self.open_datasets[file] = dataset
+        return dataset
 
     def get_source_time_ranges(self):
         file_dict = self._get_year_to_file_dict(self.dir_path)
@@ -82,10 +82,6 @@ class CEmissionsProvider(BaseImageProvider):
     def get_spatial_coverage(self):
         return -180, -90, 1440, 720
 
-    def close(self):
-        for dataset in self.open_datasets.values():
-            dataset.close()
-
     def get_variable_metadata(self, variable):
         metadata = {
             'datatype': numpy.float32,
@@ -96,6 +92,32 @@ class CEmissionsProvider(BaseImageProvider):
             'add_offset': 0.0,
         }
         return metadata
+
+    def close(self):
+        files = list(self.open_datasets.keys())
+        for file in files:
+            self._close_dataset(file)
+
+    def _close_dataset(self, file):
+        if file not in self.open_datasets:
+            return
+        self.log('closing %s' % file)
+        dataset = self.open_datasets[file]
+        dataset.close()
+        del self.open_datasets[file]
+
+    def _get_unpacked_file(self, file):
+        root, _ = os.path.splitext(file)
+        filename = os.path.basename(root)
+        real_file = os.path.join(self.temp_path, filename)
+        if not os.path.exists(real_file):
+            if not os.path.exists(self.temp_path):
+                os.makedirs(self.temp_path, exist_ok=True)
+            self.log('unpacking %s to %s' % (file, real_file))
+            with gzip.open(file, 'rb') as istream:
+                with open(real_file, 'wb') as ostream:
+                    ostream.write(istream.read())
+        return real_file
 
     @staticmethod
     def _get_year_to_file_dict(dir_path):
