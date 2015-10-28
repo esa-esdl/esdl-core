590851a7b3b3e176c63197a9ae752cdb6ef0e9e6
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2015-10-28 09:41:06 +0100

    added netcdf4 asserts

--- test/test_netcdf4.py
+++ test/test_netcdf4.py
@@ -8,30 +8,45 @@ import numpy
 
 
 class NetCDF4Test(TestCase):
-    def test_how_fill_value_is_handled_by_library(self):
+    def test_that_fill_value_from_netcdf_works_with_numpy_ma(self):
         ds = netCDF4.Dataset('test.nc', mode='w', format='NETCDF4')
-        ds.createDimension('d1', 5)
+        ds.createDimension('d1', 4)
         var = ds.createVariable('v1', 'f4', dimensions=('d1',), fill_value=-9999.0)
-        var[:] = numpy.array([1.0, -9999.0, 3.0, numpy.NaN, 5.0])
+        var[:] = numpy.array([1.0, -9999.0, 3.0, numpy.NaN])
         ds.close()
 
         ds = netCDF4.Dataset('test.nc')
         var = ds.variables['v1']
-        data = var[:]
+        array = var[:]
         ds.close()
 
-        self.assertTrue(numpy.ma.is_masked(data))
-        self.assertEqual(1.0, data[0], msg='at index 0')
-        self.assertIs(numpy.ma.masked, data[1], msg='at index 2')
-        self.assertEqual(3.0, data[2], msg='at index 3')
-        self.assertTrue(numpy.isnan(data[3]), msg='at index 4')
-        self.assertEqual(5.0, data[4], msg='at index 5')
+        # Test: The netCDF4 module will not automatically mask NaN values
+        self.assertTrue(numpy.ma.is_masked(array))
+        self.assertEqual(1.0, array[0], msg='at index 0')
+        self.assertIs(numpy.ma.masked, array[1], msg='at index 1')
+        self.assertEqual(3.0, array[2], msg='at index 2')
+        self.assertTrue(numpy.isnan(array[3]), msg='at index 3')
 
-        data2 = data + numpy.ones(5)
+        # Test: The netCDF4 module returns a masked array that can be used in expressions
+        new_array = array + numpy.ones(4)
+        self.assertTrue(numpy.ma.is_masked(new_array))
+        self.assertEqual(2.0, new_array[0], msg='at index 0')
+        self.assertIs(numpy.ma.masked, new_array[1], msg='at index 1')
+        self.assertEqual(4.0, new_array[2], msg='at index 2')
+        self.assertTrue(numpy.isnan(new_array[3]), msg='at index 3')
 
-        self.assertTrue(numpy.ma.is_masked(data2))
-        self.assertEqual(2.0, data2[0], msg='at index 0')
-        self.assertIs(numpy.ma.masked, data2[1], msg='at index 2')
-        self.assertEqual(4.0, data2[2], msg='at index 3')
-        self.assertTrue(numpy.isnan(data2[3]), msg='at index 4')
-        self.assertEqual(6.0, data2[4], msg='at index 5')
+        # Test: Mask invalid in the netCDF4 masked array
+        new_array = numpy.ma.masked_invalid(array, copy=False)
+        self.assertTrue(numpy.ma.is_masked(new_array))
+        self.assertEqual(1.0, new_array[0], msg='at index 0')
+        self.assertIs(numpy.ma.masked, new_array[1], msg='at index 1')
+        self.assertEqual(3.0, new_array[2], msg='at index 2')
+        self.assertIs(numpy.ma.masked, new_array[3], msg='at index 3')
+
+        # Test: Fill masked values in the netCDF4 masked array
+        new_array = array.filled(numpy.NaN)
+        self.assertFalse(numpy.ma.is_masked(new_array))
+        self.assertEqual(1.0, new_array[0], msg='at index 0')
+        self.assertTrue(numpy.isnan(new_array[1]), msg='at index 1')
+        self.assertEqual(3.0, new_array[2], msg='at index 2')
+        self.assertTrue(numpy.isnan(new_array[3]), msg='at index 3')
