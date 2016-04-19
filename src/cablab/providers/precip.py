82324db22842ef1ee4970215ea6580efa9dd50f3
Hans Permana <hans.permana@gmail.com>
2016-04-19 15:58:56 +0200

    enabled high res cube generator for provider precipitation.

--- src/cablab/providers/precip.py
+++ src/cablab/providers/precip.py
@@ -3,19 +3,18 @@ from datetime import timedelta
 
 import numpy
 import netCDF4
+import gridtools.resampling as gtr
 
 from cablab import BaseCubeSourceProvider
 from cablab.util import NetCDFDatasetCache, aggregate_images
 
 VAR_NAME = 'Precip'
+FILL_VALUE = -9999.0
 
 
 class PrecipProvider(BaseCubeSourceProvider):
     def __init__(self, cube_config, dir_path):
         super(PrecipProvider, self).__init__(cube_config)
-        if cube_config.grid_width != 1440 or cube_config.grid_height != 720:
-            raise ValueError('illegal cube configuration, '
-                             'provider does not yet implement proper spatial aggregation/interpolation')
         self.dir_path = dir_path
         self.source_time_ranges = None
         self.dataset_cache = NetCDFDatasetCache(VAR_NAME)
@@ -28,7 +27,7 @@ class PrecipProvider(BaseCubeSourceProvider):
         return {
             VAR_NAME: {
                 'data_type': numpy.float32,
-                'fill_value': -9999.0,
+                'fill_value': FILL_VALUE,
                 'units': 'mm/day',
                 'long_name': 'Precip - V1.0',
                 'standard_name': 'Precip',
@@ -67,6 +66,8 @@ class PrecipProvider(BaseCubeSourceProvider):
                 j += 1
             precip = aggregate_images(images, weights=weights)
 
+        precip = gtr.resample2d(precip, self.cube_config.grid_width, self.cube_config.grid_height,
+                                us_method=gtr.US_NEAREST, fill_value=FILL_VALUE)
         return {VAR_NAME: precip}
 
     def _get_file_and_time_index(self, i):
@@ -76,7 +77,7 @@ class PrecipProvider(BaseCubeSourceProvider):
         return self.source_time_ranges
 
     def get_spatial_coverage(self):
-        return 0, 0, 1440, 720
+        return 0, 0, self.cube_config.grid_width, self.cube_config.grid_height
 
     def close(self):
         self.dataset_cache.close_all_datasets()
--- test/providers/test_precip.py
+++ test/providers/test_precip.py
@@ -45,3 +45,13 @@ class PrecipProviderTest(unittest.TestCase):
         self.assertTrue('Precip' in images)
         image = images['Precip']
         self.assertEqual((720, 1440), image.shape)
+
+    @unittest.skipIf(not os.path.exists(SOURCE_DIR), 'test data not found: ' + SOURCE_DIR)
+    def test_get_high_res_images(self):
+        provider = PrecipProvider(CubeConfig(grid_width=4320, grid_height=2160, spatial_res=1/12), SOURCE_DIR)
+        provider.prepare()
+        images = provider.compute_variable_images(datetime(1996, 1, 1), datetime(1996, 1, 9))
+        self.assertIsNotNone(images)
+        self.assertTrue('Precip' in images)
+        image = images['Precip']
+        self.assertEqual((2160, 4320), image.shape)
