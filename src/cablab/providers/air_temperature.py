d8ce591defb7e85c087de97bb5a7fe90fc014546
Hans Permana <hans.permana@gmail.com>
2016-04-20 17:37:17 +0200

    enabled high res cube generator for provider air temperature.

--- src/cablab/providers/air_temperature.py
+++ src/cablab/providers/air_temperature.py
@@ -3,20 +3,18 @@ import os
 
 import numpy
 import netCDF4
+import gridtools.resampling as gtr
 
 from cablab import BaseCubeSourceProvider
 from cablab.util import NetCDFDatasetCache, aggregate_images
 
 VAR_NAME = 't2m'
+FILL_VALUE = -32767
 
 
 class AirTemperatureProvider(BaseCubeSourceProvider):
     def __init__(self, cube_config, dir_path):
         super(AirTemperatureProvider, self).__init__(cube_config)
-        # todo (nf 20151028) - remove check once we have addressed spatial aggregation/interpolation, see issue #3
-        if cube_config.grid_width != 1440 or cube_config.grid_height != 720:
-            raise ValueError('illegal cube configuration, '
-                             'provider does not yet implement proper spatial aggregation/interpolation')
         self.dir_path = dir_path
         self.source_time_ranges = None
         self.dataset_cache = NetCDFDatasetCache(VAR_NAME)
@@ -29,7 +27,7 @@ class AirTemperatureProvider(BaseCubeSourceProvider):
         return {
             VAR_NAME: {
                 'data_type': numpy.float32,
-                'fill_value': -32767,
+                'fill_value': FILL_VALUE,
                 'units': 'K',
                 'long_name': '2 metre temperature',
                 'scale_factor': 0.0019718202938428923,
@@ -51,21 +49,20 @@ class AirTemperatureProvider(BaseCubeSourceProvider):
         if len(new_indices) == 1:
             i = next(iter(new_indices))
             file, time_index = self._get_file_and_time_index(i)
-            dataset = self.dataset_cache.get_dataset(file)
-            air_temperature = dataset.variables[VAR_NAME][time_index, 0:720, :]
+            air_temperature = self.dataset_cache.get_dataset(file).variables[VAR_NAME][time_index, 0:720, :]
         else:
             images = [None] * len(new_indices)
             weights = [None] * len(new_indices)
             j = 0
             for i in new_indices:
                 file, time_index = self._get_file_and_time_index(i)
-                dataset = self.dataset_cache.get_dataset(file)
-                variable = dataset.variables[VAR_NAME]
-                images[j] = variable[time_index, 0:720, :]
+                images[j] = self.dataset_cache.get_dataset(file).variables[VAR_NAME][time_index, 0:720, :]
                 weights[j] = index_to_weight[i]
                 j += 1
             air_temperature = aggregate_images(images, weights=weights)
 
+        air_temperature = gtr.resample2d(air_temperature, self.cube_config.grid_width, self.cube_config.grid_height,
+                                         us_method=gtr.US_NEAREST, fill_value=FILL_VALUE)
         return {VAR_NAME: air_temperature}
 
     def _get_file_and_time_index(self, i):
@@ -75,7 +72,7 @@ class AirTemperatureProvider(BaseCubeSourceProvider):
         return self.source_time_ranges
 
     def get_spatial_coverage(self):
-        return 0, 0, 1440, 720
+        return 0, 0, self.cube_config.grid_width, self.cube_config.grid_height
 
     def close(self):
         self.dataset_cache.close_all_datasets()
