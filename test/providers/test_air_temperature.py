82ca9390a3649c2519e9eac3926dae69541386da
gunbra32 <gun.brandt@googlemail.com>
2016-06-01 15:21:58 +0200

    Modified test to work with monthly input files.

--- test/providers/test_air_temperature.py
+++ test/providers/test_air_temperature.py
@@ -6,28 +6,29 @@ from cablab import CubeConfig
 from cablab.providers.air_temperature import AirTemperatureProvider
 from cablab.util import Config
 
-SOURCE_DIR = Config.instance().get_cube_source_path('T2m-ECMWF\\low')
-
+SOURCE_DIR = Config.instance().get_cube_source_path("T2m-ECMWF/low/")
+print(SOURCE_DIR)
 
 class AirTemperatureProviderTest(unittest.TestCase):
     @unittest.skipIf(not os.path.exists(SOURCE_DIR), 'test data not found: ' + SOURCE_DIR)
     def test_source_time_ranges(self):
-        provider = AirTemperatureProvider(CubeConfig(end_time=datetime(2001, 6, 1, 0, 0)), dir=SOURCE_DIR)
+        provider = AirTemperatureProvider(CubeConfig(end_time=datetime(2001, 12, 31, 23, 0)), dir=SOURCE_DIR)
         provider.prepare()
         source_time_ranges = provider.source_time_ranges
-        self.assertEqual(1460, len(source_time_ranges))
+        print(source_time_ranges)
+        self.assertEqual(730, len(source_time_ranges))
         self.assertEqual((datetime(2001, 1, 1, 0, 0),
-                          datetime(2001, 1, 1, 6, 0),
-                          os.path.join(SOURCE_DIR, 'W:\\T2m-ECMWF\\low\\t2m_2001.nc'),
+                          datetime(2001, 1, 1, 12, 0),
+                          os.path.join(SOURCE_DIR, 't2m_2001_01.nc'),
                           0), source_time_ranges[0])
-        self.assertEqual((datetime(2001, 1, 2, 12, 0),
-                          datetime(2001, 1, 2, 18, 0),
-                          os.path.join(SOURCE_DIR, 'W:\\T2m-ECMWF\\low\\t2m_2001.nc'),
+        self.assertEqual((datetime(2001, 1, 4, 0, 0),
+                          datetime(2001, 1, 4, 12, 0),
+                          os.path.join(SOURCE_DIR, 't2m_2001_01.nc'),
                           6), source_time_ranges[6])
-        self.assertEqual((datetime(2001, 12, 31, 18, 0),
+        self.assertEqual((datetime(2001, 12, 31, 12, 0),
                           datetime(2002, 1, 1, 0, 0),
-                          os.path.join(SOURCE_DIR, 'W:\\T2m-ECMWF\\low\\t2m_2001.nc'),
-                          1459), source_time_ranges[1459])
+                          os.path.join(SOURCE_DIR, 't2m_2001_12.nc'),
+                          61), source_time_ranges[729])
 
     @unittest.skipIf(not os.path.exists(SOURCE_DIR), 'test data not found: ' + SOURCE_DIR)
     def test_temporal_coverage(self):
