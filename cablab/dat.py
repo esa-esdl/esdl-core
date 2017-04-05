16560a980f47368e475fe75ddd646e7521279c24
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2017-04-05 16:27:37 +0200

    initial DAT (xarray-based utilities)

---
+++ cablab/dat.py
@@ -0,0 +1,12 @@
+"""
+ESDC DAT (Data Analytics Toolkit)
+=================================
+
+Note, the ESDC (Earth System Data Cube) is accessed via an 
+`xarray.Dataset <http://xarray.pydata.org/en/stable/data-structures.html#dataset>_`, therefore the ``xarray.Dataset``
+API is the primary DAT.
+
+This module provides additional analytical utility functions which work for `xarray.Dataset`, `xarray.DataArray`, and
+also Numpy arrays. 
+"""
+
