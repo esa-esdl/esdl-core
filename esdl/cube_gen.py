836f4d2f1bdf9e3d23cf9cf1ceecb379d10189ee
Helge <helge.dzierzon@brockmann-consult.de>
2018-10-05 15:29:16 +0200

    Revert "Shifted an import to teh top of the script"
    This reverts commit 8c24141d8161e8e20cee589ae2793d4bfd398cb4.

--- esdl/cube_gen.py
+++ esdl/cube_gen.py
@@ -3,7 +3,6 @@ import os
 import sys
 
 from pkg_resources import iter_entry_points
-from collections import OrderedDict
 
 from .version import version as __version__
 from .cube import Cube
@@ -27,6 +26,7 @@ SOURCE_PROVIDERS = _load_source_providers()
 
 
 def _parse_source_arg(source: str):
+    from collections import OrderedDict
     parts = source.split(os.pathsep)
     name = parts[0]
     if not name:
