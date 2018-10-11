c958fd5a214ae5f3c10d68a4d64d1898692c22d4
Helge <helge.dzierzon@brockmann-consult.de>
2018-10-11 15:24:34 +0200

    Changed Data Source Cahche to XarrayDataSourceCache which is linked to cate

--- esdl/providers/aerosols.py
+++ esdl/providers/aerosols.py
@@ -4,10 +4,10 @@ from datetime import timedelta
 
 import numpy
 
-from esdl.cube_provider import NetCDFCubeSourceProvider
+from esdl.cube_provider import CateCubeSourceProvider
 
 
-class AerosolsProvider(NetCDFCubeSourceProvider):
+class AerosolsProvider(CateCubeSourceProvider):
     def __init__(self, cube_config, name='aerosols', dir=None, resampling_order=None):
         super(AerosolsProvider, self).__init__(cube_config, name, dir, resampling_order)
         self.old_indices = None
@@ -15,8 +15,8 @@ class AerosolsProvider(NetCDFCubeSourceProvider):
     @property
     def variable_descriptors(self):
         return {
-            'aerosol_optical_thickness_1610': {
-                'source_name': 'AOD1610_mean',
+            'AAOD550_mean': {
+                'source_name': 'AAOD550_mean',
                 'data_type': numpy.float32,
                 'fill_value': -999.0,
                 'units': '1',
@@ -25,74 +25,22 @@ class AerosolsProvider(NetCDFCubeSourceProvider):
                 'references': 'Holzer-Popp, T., de Leeuw, G., Griesfeller, J., Martynenko, D., Klueser, L., Bevan, S., et al. (2013). Aerosol retrieval experiments in the ESA Aerosol_cci project. Atmospheric Measurement Techniques, 6, 1919-1957. doi:10.5194/amt-6-1919-2013. ',
                 'comment': 'Aerosol optical thickness derived from the dataset produced by the Aerosol CCI project.',
                 'url': 'http://www.esa-aerosol-cci.org/',
-                'project_name' : 'ESA Aerosol CCI',
+                'project_name': 'ESA Aerosol CCI',
             },
-            'aerosol_optical_thickness_550': {
-                'source_name': 'AOD550_mean',
-                'data_type': numpy.float32,
-                'fill_value': -999.0,
-                'units': '1',
-                'long_name': 'Aerosol Optical Thickness at 550 nm',
-                'standard_name': 'atmosphere_optical_thickness_due_to_aerosol_at_550nm',
-                'references': 'Holzer-Popp, T., de Leeuw, G., Griesfeller, J., Martynenko, D., Klueser, L., Bevan, S., et al. (2013). Aerosol retrieval experiments in the ESA Aerosol_cci project. Atmospheric Measurement Techniques, 6, 1919-1957. doi:10.5194/amt-6-1919-2013. ',
-                'comment': 'Aerosol optical thickness derived from the dataset produced by the Aerosol CCI project.',
-                'url': 'http://www.esa-aerosol-cci.org/',
-                'project_name' : 'ESA Aerosol CCI',
-            },
-            'aerosol_optical_thickness_555': {
-                'source_name': 'AOD555_mean',
-                'data_type': numpy.float32,
-                'fill_value': -999.0,
-                'units': '1',
-                'long_name': 'Aerosol Optical Thickness at 555 nm',
-                'standard_name': 'atmosphere_optical_thickness_due_to_aerosol_at_555nm',
-                'references': 'Holzer-Popp, T., de Leeuw, G., Griesfeller, J., Martynenko, D., Klueser, L., Bevan, S., et al. (2013). Aerosol retrieval experiments in the ESA Aerosol_cci project. Atmospheric Measurement Techniques, 6, 1919-1957. doi:10.5194/amt-6-1919-2013. ',
-                'comment': 'Aerosol optical thickness derived from the dataset produced by the Aerosol CCI project.',
-                'url': 'http://www.esa-aerosol-cci.org/',
-                'project_name' : 'ESA Aerosol CCI',
-            },
-            'aerosol_optical_thickness_659': {
-                'source_name': 'AOD659_mean',
-                'data_type': numpy.float32,
-                'fill_value': -999.0,
-                'units': '1',
-                'long_name': 'Aerosol Optical Thickness at 659 nm',
-                'standard_name': 'atmosphere_optical_thickness_due_to_aerosol_at_659nm',
-                'references': 'Holzer-Popp, T., de Leeuw, G., Griesfeller, J., Martynenko, D., Klueser, L., Bevan, S., et al. (2013). Aerosol retrieval experiments in the ESA Aerosol_cci project. Atmospheric Measurement Techniques, 6, 1919-1957. doi:10.5194/amt-6-1919-2013. ',
-                'comment': 'Aerosol optical thickness derived from the dataset produced by the Aerosol CCI project.',
-                'url': 'http://www.esa-aerosol-cci.org/',
-                'project_name' : 'ESA Aerosol CCI',
-            },
-            'aerosol_optical_thickness_865': {
-                'source_name': 'AOD865_mean',
-                'data_type': numpy.float32,
-                'fill_value': -999.0,
-                'units': '1',
-                'long_name': 'Aerosol Optical Thickness at 865 nm',
-                'standard_name': 'atmosphere_optical_thickness_due_to_aerosol_at_865nm',
-                'references': 'Holzer-Popp, T., de Leeuw, G., Griesfeller, J., Martynenko, D., Klueser, L., Bevan, S., et al. (2013). Aerosol retrieval experiments in the ESA Aerosol_cci project. Atmospheric Measurement Techniques, 6, 1919-1957. doi:10.5194/amt-6-1919-2013. ',
-                'comment': 'Aerosol optical thickness derived from the dataset produced by the Aerosol CCI project.',
-                'url': 'http://www.esa-aerosol-cci.org/',
-                'project_name' : 'ESA Aerosol CCI',
-            }
         }
 
     def compute_source_time_ranges(self):
         source_time_ranges = []
         for root, sub_dirs, files in os.walk(self.dir_path):
-            for sub_dir in sub_dirs:
-                source_year = int(sub_dir)
-                if self.cube_config.start_time.year <= source_year <= self.cube_config.end_time.year:
-                    sub_dir_path = os.path.join(self.dir_path, sub_dir)
-                    file_names = os.listdir(sub_dir_path)
-                    for file_name in file_names:
-                        time_info = file_name.split('-', 1)[0]
-                        time = self.day2date(int(time_info))
-                        if self.cube_config.start_time <= time <= self.cube_config.end_time:
-                            file = os.path.join(sub_dir_path, file_name)
-                            self.dataset_cache.get_dataset(file)
-                            self.dataset_cache.close_dataset(file)
-                            source_time_ranges.append((time, time + timedelta(days=1), file, 0))
+            for file_name in files:
+                time_info = file_name.split('-', 1)[0]
+
+                time = self.day2date(int(time_info))
+                if self.cube_config.start_time <= time <= self.cube_config.end_time:
+                    file = os.path.join(root, file_name)
+                    self.dataset_cache.get_dataset(file)
+                    self.dataset_cache.close_dataset(file)
+                    source_time_ranges.append((time, time + timedelta(days=1), file, 0))
         return sorted(source_time_ranges, key=lambda item: item[0])
 
     def transform_source_image(self, source_image):
