c87abcdbabf3927e9d6964defa97f03eef76e9d9
Helge <helge.dzierzon@brockmann-consult.de>
2018-10-19 16:07:43 +0200

    CHanged some assertEquals to assertEqual

--- test/test_cube_gen.py
+++ test/test_cube_gen.py
@@ -12,13 +12,13 @@ class MainTest(TestCase):
         # todo (nf 20160512) - fetch stdout and verify
 
     def test_parse_source_arg(self):
-        self.assertEquals(('', None, None, 'SOURCE name must not be empty'), _parse_source_arg(''))
-        self.assertEquals(('', None, None, 'SOURCE name must not be empty'), _parse_source_arg(os.pathsep))
-        self.assertEquals(('', None, None, 'SOURCE name must not be empty'), _parse_source_arg(os.pathsep + 'a'))
-        self.assertEquals(('a', [['']], OrderedDict(), None), _parse_source_arg('a' + os.pathsep))
-        self.assertEquals(('a', [['b']], OrderedDict(), None), _parse_source_arg('a' + os.pathsep + 'b'))
-        self.assertEquals(('a', [['b']], OrderedDict([('c', '8')]), None),
+        self.assertEqual(('', None, None, 'SOURCE name must not be empty'), _parse_source_arg(''))
+        self.assertEqual(('', None, None, 'SOURCE name must not be empty'), _parse_source_arg(os.pathsep))
+        self.assertEqual(('', None, None, 'SOURCE name must not be empty'), _parse_source_arg(os.pathsep + 'a'))
+        self.assertEqual(('a', [['']], OrderedDict(), None), _parse_source_arg('a' + os.pathsep))
+        self.assertEqual(('a', [['b']], OrderedDict(), None), _parse_source_arg('a' + os.pathsep + 'b'))
+        self.assertEqual(('a', [['b']], OrderedDict([('c', '8')]), None),
                           _parse_source_arg('a' + os.pathsep + 'b' + os.pathsep + 'c=8'))
-        self.assertEquals(('a', [['c'], ['e']], OrderedDict([('b', '1'), ('d', '2')]), None),
+        self.assertEqual(('a', [['c'], ['e']], OrderedDict([('b', '1'), ('d', '2')]), None),
                           _parse_source_arg(
                               'a' + os.pathsep + 'b=1' + os.pathsep + 'c' + os.pathsep + 'd=2' + os.pathsep + 'e'))
